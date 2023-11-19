7b59141cab79f28810d4e21e302a9b15b66623fd
Eren Çolak <eren.colak01@gmail.com>
2023-11-19 03:58:40 +0300

    actorControlEnd

---
+++ FilmWebApi.BLL/DTO/ActorDTO/UpdateMovieInActorDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilmWebApi.BLL.DTO.ActorDTO
+{
+    public class UpdateMovieInActorDTO
+    {
+
+        public int OldFilmId { get; set; }
+        public int NewFilmId { get; set; }
+
+
+    }
+}
